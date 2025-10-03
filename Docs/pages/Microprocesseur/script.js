dab0ed7accdcf6721f8d9b9229dedd73a0a269d7
Zolex <phaeussermann@EDUHDF.FR>
2025-10-03 12:23:19 +0200

    maj micro p

--- Docs/pages/Microprocesseur/script.js
+++ Docs/pages/Microprocesseur/script.js
@@ -12,3 +12,36 @@ document.querySelectorAll('.zoomable').forEach(img => {
 overlay.addEventListener('click', () => {
     overlay.classList.remove('show');
 });
+
+const button = document.getElementById('backToTop');
+
+  // affiche le bouton quand on descend
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 700) { 
+      button.style.display = 'block';
+    } else {
+      button.style.display = 'none';
+    }
+  });
+
+  // remonter en haut 
+  button.addEventListener('click', () => {
+    window.scrollTo({
+      top: 0,           
+      behavior: 'smooth' 
+    });
+  });
+
+  const toggleButton = document.getElementById('themeToggle');
+  const body = document.body;
+
+  toggleButton.addEventListener('click', () => {
+    body.classList.toggle('dark-mode'); // ajoute ou retire la classe dark-mode
+
+    // On change l’icône du bouton
+    if (body.classList.contains('dark-mode')) {
+      toggleButton.textContent = '☀️'; // soleil pour mode clair
+    } else {
+      toggleButton.textContent = '🌙'; // lune pour mode sombre
+    }
+  });
